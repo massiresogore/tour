6d4ed7b6da7698ba9d9361e413f0c37e35f1c1c6
sogore massire <massire.org@gmail.com>
2023-04-10 18:46:13 +0200

    add more and less fonctionality

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Tour = ({ id, image, name, price, info, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
   return (
     <article className="single-tour" key={id}>
       <img src={image} alt={name} className="img" />
       <span className="tour-price">${price}</span>
       <div className="tour-info">
         <h2>{name}</h2>
-        <p>{info}</p>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button
+            className="info-btn"
+            type="button"
+            onClick={() => {
+              setReadMore(!readMore);
+            }}
+          >
+            {readMore ? "show less" : "read more"}
+          </button>
+        </p>
         <button
           className="btn btn-block delete-btn"
           onClick={() => {
